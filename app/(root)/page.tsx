9f20fea95b2080040126d46a7da6c9cb7e156525
Lucas Freire <lucasfreirel.dev@gmail.com>
2025-04-27 18:49:12 -0300

    feat: implement hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,15 @@
-
 export default function Home() {
-
   return (
     <>
-      Hello World - Home
+      <section className="pink_container">
+        <h1 className="heading">Monte Sua Startup, <br /> Conecte-se com Empreendedores</h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Acelere sua jornada empreendedora com nossa plataforma de conexão e aprendizado.
+          Encontre mentores, faça networking e impulsione seu negócio.
+          Junte-se a nós e transforme sua ideia em realidade!
+        </p>
+      </section>
     </>
   );
 }
