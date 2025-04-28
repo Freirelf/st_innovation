bbc6af42684e24ec787cb46f0c22ba8db82b1618
Lucas Freire <lucasfreirel.dev@gmail.com>
2025-04-28 19:04:39 -0300

    feat: startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import { auth } from "@/auth";
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -6,6 +8,18 @@ export default async function Home({
   searchParams: Promise<{ query: string }>;
 }) {
   const query = (await searchParams).query || "";
+
+  const posts = [{
+    _createAt: new Date(),
+    views: 100,
+    author: { _id: 1, name: "John Doe" },
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    image: "http://images.unsplash.com/photo-1593376853899-fbb47a057fa0?q=80&w=1915&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D://images.unsplash.com/photo-1677631231234-1234567890",
+    category: "Robótica",
+    title: "Startup de Robótica",
+    _id: 1,
+  }]
 
   return (
     <>
@@ -21,6 +35,25 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold" >
+          {query ? `Resultados para "${query}"` : "Todas as Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+            {posts?.length > 0 ? (
+                posts.map((post: StartupCardType) => (
+                    <StartupCard key={post?._id} post={post} />
+                ))
+            ) : (
+                <div className="flex flex-col items-center justify-center w-full h-full">
+                    <h2 className="text-2xl font-semibold text-gray-700">Nenhum resultado encontrado</h2>
+                    <p className="text-gray-500">Tente outra pesquisa ou crie sua própria startup.</p>
+                </div>
+            )}
+        </ul>
       </section>
     </>
   );
