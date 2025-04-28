a0cd9abe7f1495610fc5f33d169f1c5f07f0c083
Lucas Freire <lucasfreirel.dev@gmail.com>
2025-04-27 21:58:42 -0300

    feat: implement search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,26 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query: string }>;
+}) {
+  const query = (await searchParams).query || "";
+
   return (
     <>
       <section className="pink_container">
-        <h1 className="heading">Monte Sua Startup, <br /> Conecte-se com Empreendedores</h1>
+        <h1 className="heading">
+          Monte Sua Startup, <br /> Conecte-se com Empreendedores
+        </h1>
 
         <p className="sub-heading !max-w-3xl">
-          Acelere sua jornada empreendedora com nossa plataforma de conexão e aprendizado.
-          Encontre mentores, faça networking e impulsione seu negócio.
-          Junte-se a nós e transforme sua ideia em realidade!
+          Acelere sua jornada empreendedora com nossa plataforma de conexão e
+          aprendizado. Encontre mentores, faça networking e impulsione seu
+          negócio. Junte-se a nós e transforme sua ideia em realidade!
         </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
